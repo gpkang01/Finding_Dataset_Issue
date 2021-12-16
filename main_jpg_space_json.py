2acc92cd0260d639d81586d35b47dab34f213638
gpkang01 <gpkang01@gmail.com>
2021-12-16 15:40:11 +0900

    seperate json path

--- main_jpg_space_json.py
+++ main_jpg_space_json.py
@@ -26,6 +26,7 @@ print(len(space_img_lst), len(set_space_img_lst))
 # read same image file
 if not os.path.isfile('{}/same_jpg_class_ins.csv'.format(original_path)):
     make_final_jpg_csv()
+print('중복이미지 리스트에서 인스턴스 리스트를 추출 중입니다.')
 same_img_lst = []
 f = open('{}/same_jpg_class_ins.csv'.format(original_path), 'r', encoding = 'CP949')
 f_field_name = ['작업 경로', '이전 파일명', '변경 파일명', '참고 사항']
@@ -42,6 +43,7 @@ same_json_lst = []
 same_json_name_lst = []
 if not os.path.isfile('{}/same_json_class_ins.csv'.format(original_path)):
     make_final_json_csv()
+print('중복 JSON 리스트에서 인스턴스 리스트를 추출 중입니다.')
 f = open('{}/same_json_class_ins.csv'.format(original_path), 'r', encoding = 'CP949')
 f_field_name = ['파일 경로', '파일 이름']
 change_line = csv.DictReader(f, fieldnames = f_field_name)
@@ -52,6 +54,7 @@ for line in change_line:
         same_json_name_lst.append(line['파일 이름'].strip().split('.')[0])
         same_json_lst.append([line['파일 경로'], line['파일 이름']])
 
+print('추출된 데이타를 통합 작성 중입니다.')
 # 중복 이미지와 허공 이미지 겹치는 list
 same_img_space_lst = []
 for ilst in same_img_lst:
@@ -75,19 +78,19 @@ for islst in same_img_space_lst:
     if islst[1].strip().split('.')[0] in same_json_name_lst:
         index = same_json_name_lst.index(islst[1].strip().split('.')[0])
         same_img_space_json_lst.append([islst[0], islst[1], islst[2], islst[3], islst[4],\
-                                       '{}/{}'.format(same_json_lst[index][0], same_json_lst[index][1]), change_json_name])
+                                       same_json_lst[index][0], same_json_lst[index][1], change_json_name])
         same_json_name_lst.remove(same_json_name_lst[index])
         same_json_lst.remove(same_json_lst[index])
     else:
-        same_img_space_json_lst.append([islst[0], islst[1], islst[2], islst[3], islst[4], '파일 없음', ''])
+        same_img_space_json_lst.append([islst[0], islst[1], islst[2], islst[3], islst[4], '경로 없음', '파일 없음', '없음'])
     print(count)
 
 f = open('{}/main_jpg_space_json.csv'.format(original_path), 'w', encoding = 'CP949', newline = '')
-f_field_name = ['이미지 경로', '이전 파일명', '변경 파일명', '참고 사항', '허공 이미지', 'JSON 경로', '변경 JSON명']
+f_field_name = ['이미지 경로', '이전 파일명', '변경 파일명', '참고 사항', '허공 이미지', 'JSON 경로', '이전 JSON명', '변경 JSON명']
 f_writer = csv.DictWriter(f, fieldnames = f_field_name)
 f_writer.writeheader()
 for lst in same_img_space_json_lst:
     line = {'이미지 경로' : lst[0], '이전 파일명' : lst[1], '변경 파일명' : lst[2], '참고 사항' : lst[3],\
-            '허공 이미지' : lst[4], 'JSON 경로' : lst[5], '변경 JSON명' : lst[6]}
+            '허공 이미지' : lst[4], 'JSON 경로' : lst[5], '이전 JSON명' : lst[6], '변경 JSON명' : lst[7]}
     f_writer.writerow(line)
 f.close()
